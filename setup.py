79f91faaa2cfd16f2f84266d209f53c308f1d04a
Matt Hollingsworth <matt@deepfield.net>
2015-05-26 21:19:47 -0400

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='builder',
+      version='0.2.0',
+      description='A framework for building batch processing pipelines',
+      author='Max Mizikar, Matt Hollingsworth',
+      url='http://github.com/deepfield/builder',
+      packages=['builder']
+)
