339d2b43a7902d0ce66cafc35f6cd90581e08504
Matt Hollingsworth <matt@deepfield.net>
2015-06-09 11:44:27 -0400

    Add all dependencies

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='builder',
       author='Max Mizikar, Matt Hollingsworth',
       url='http://github.com/deepfield/builder',
       packages=find_packages(),
-      install_requires=['python-dateutil', 'arrow>=0.4.3']
+      install_requires=['python-dateutil', 'arrow>=0.4.3', 'networkx>=1.8.1', 'futures>=2.1.6', 'tornado >= 3.2.2']
 )
