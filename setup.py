2ac5c474238f79d5164b98cb681ce3795f90922b
Matt Hollingsworth <matt@deepfield.net>
2015-06-22 14:45:34 -0400

    Set download URL

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='builder',
       description='A framework for building batch processing pipelines',
       author='Max Mizikar, Matt Hollingsworth',
       url='http://github.com/deepfield/builder',
+      download_url='https://github.com/deepfield/builder/tarball/0.1.0',
       packages=find_packages(),
       install_requires=['python-dateutil', 'arrow>=0.4.3', 'networkx>=1.8.1', 'futures>=2.1.6', 'tornado >= 3.2.2']
 )
